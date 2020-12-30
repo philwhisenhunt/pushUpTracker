8d46b88bda21d85c29340d4895373e5081d20191
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-12-29 19:06:01 -0500

    First attempt

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <View style={StyleSheet.container}>
+      <Text> You did {count} push-ups</Text>
+      <Button
+      onPress={()} => setCount(count + 1)}
+      title="Click me!"
+    />
+    </View>
+  );
+};
+
+// This is where the react Native Styles are
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
