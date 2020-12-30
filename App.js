63172b5c938d7f3acd1ad73f2bdcf07073de73b7
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-12-29 19:22:00 -0500

    Push up tracker cobbled together

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
-
-const App = () => {
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
     <View style={styles.container}>
-      <Text>You did {count} push-ups</Text>
-      <Button onPress={() => setCount(count + 1)} title="Click me!" />
+      <Text>Push up tracker</Text>
+      <Text>You did {count} push-ups!</Text>
+      <Button onPress={() => setCount(count + 1)} title="Log a push-up!" />
     </View>
   );
-};
+}
 
-// This is where the react Native Styles are
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    backgroundColor: "#fff",
     alignItems: "center",
+    justifyContent: "center",
   },
 });
