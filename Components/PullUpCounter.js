0f994083ea11bdca355ab6405f4a2ce6b19e3a6e
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-12-30 21:54:29 -0500

    Add rest of component

--- Components/PullUpCounter.js
+++ Components/PullUpCounter.js
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 const PullUpCounter = ({ title, buttonTitle }) => {
-    const [pullupCount, setPullupCount] = useState(10);
+  const [pullupCount, setPullupCount] = useState(10);
 
-    return (
-        <>
-        <Text>{title}</Text>
-    )
+  return (
+    <>
+      <Text>{title}</Text>
+      <Text>You did {pullupCount} pull-ups!</Text>
+      <Button
+        onPress={() => setPullupCount(pullupCount + 1)}
+        title={buttonTitle}
+      />
+    </>
+  );
 };
+export default PullUpCounter;
