59f8ca7306750df01a849313e9887d66c76d4226
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2020-12-30 21:52:15 -0500

    Establish constants, begin adding text

--- Components/PullUpCounter.js
+++ Components/PullUpCounter.js
@@ -1,3 +1,10 @@
 import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
-const PullUpCounter = ({ title, buttonTitle }) => {};
+const PullUpCounter = ({ title, buttonTitle }) => {
+    const [pullupCount, setPullupCount] = useState(10);
+
+    return (
+        <>
+        <Text>{title}</Text>
+    )
+};
