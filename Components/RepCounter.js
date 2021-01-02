91fe645c18199840c1ce8f2d016329dae276bb55
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-01-01 21:39:31 -0500

    Progress on repCounter

--- Components/RepCounter.js
+++ Components/RepCounter.js
@@ -1,2 +1,12 @@
 import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+const repCounter = ({ title, buttonTitle, repType }) => {
+    const [repCount, setRepCount] = useState(10);
+
+    return (
+        <>
+        <Button
+        onPress={() => setRepCount}
+        </>
+    )
+}
