669050618ff3ac964464ab33ea3c2cc68c31e535
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-01-05 22:14:13 -0500

    Remove old comment

--- Components/RepCounter.js
+++ Components/RepCounter.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
 const repCounter = ({ title, buttonTitle, repType }) => {
   const [repCount, setRepCount] = useState(10);
-  //do I need to import repType here?
 
   return (
     <>
