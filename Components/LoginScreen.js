d799e0d3ea948b585cb664856539b6d624614a6f
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-01-10 20:10:20 -0500

    Create LoginScreen.js

---
+++ Components/LoginScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet, Text, View } from 'react-native';
+
+state={
+    email: "",
+    password:""
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+});
