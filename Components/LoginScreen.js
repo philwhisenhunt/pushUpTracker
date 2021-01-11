47a2154a7655fb3c63fb74a8aeaca7fbbef50c16
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-01-11 18:58:32 -0500

    Add more inputs to the login screen

--- Components/LoginScreen.js
+++ Components/LoginScreen.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default class LoginScreen extends React.Component {
   render() {
@@ -13,6 +19,21 @@ export default class LoginScreen extends React.Component {
             placeholderTextColor="#003f5"
             onChangeText={(text) => this.setState({ email: text })}
           />
+          <TextInput
+            style={styles.secureTextEntry}
+            placeholder="Password"
+            placeholderTextColor="#003f5c"
+            onChangeText={(text) => this.setState({ password: text })}
+          />
+          <TouchableOpacity>
+            <Text style={styles.forgot}>Forgot Password?</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.loginText}>Signup</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.loginBtn}>
+            <Text style={styles.loginText}>LOGIN</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -29,5 +50,32 @@ const styles = StyleSheet.create({
     backgroundColor: "#003f5c",
     alignItems: "center",
     justifyContent: "center",
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "#465881",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: "white",
+  },
+  forgot: {
+    color: "white",
+    fontSize: 11,
+  },
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#fb5b5a",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10,
   },
 });
