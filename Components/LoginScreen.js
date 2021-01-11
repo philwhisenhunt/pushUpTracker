175579a9827e900356fe0f2163db33209a52578a
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-01-11 18:45:57 -0500

    Add email input

--- Components/LoginScreen.js
+++ Components/LoginScreen.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import {StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default class LoginScreen extends React.Component {
-    render(){
-        return (
-            <View style={styles.container}>
-                <Text>Text will go here.</Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Workout Tracker</Text>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.inputText}
+            placeholder="Email"
+            placeholderTextColor="#003f5"
+            onChangeText={(text) => this.setState({ email: text })}
+          />
+        </View>
+      </View>
+    );
+  }
 }
-state={
-    email: "",
-    password:""
-}
-
-<View style={styles.inputView} >
+state = {
+  email: "",
+  password: "",
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#003f5c',
-        alignItems: 'center',
-        justifyContent: 'center',
-
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#003f5c",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
