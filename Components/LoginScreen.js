ad3028007920405347e83a75678880f1011fb95a
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-01-11 18:32:56 -0500

    Adding default stuff back to get login screen going

--- Components/LoginScreen.js
+++ Components/LoginScreen.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import {StyleSheet, Text, View } from 'react-native';
 
+export default class LoginScreen extends React.Component {
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text>Text will go here.</Text>
+            </View>
+        )
+    }
+}
 state={
     email: "",
     password:""
