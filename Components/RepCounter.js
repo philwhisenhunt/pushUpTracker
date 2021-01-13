de7e7fb71272181067d298bd443c995bd9af0cfd
Adam Roth <adam.roth@slope.io>
2021-01-13 16:40:34 -0500

    Create a global RepStore and use it across components.

--- Components/RepCounter.js
+++ Components/RepCounter.js
@@ -1,25 +1,29 @@
 import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
-import { store, view } from "@risingstack/react-easy-state";
-const repCounter = ({ title, buttonTitle, repType }) => {
-  const [repCount, setRepCount] = useState(0);
-  const user = store({ name: "Phil's " + title });
-  const globalRepCount = store({ amount: 10 });
+import { view } from "@risingstack/react-easy-state";
+import RepStore from "./../Stores/RepStore";
+
+const repCounter = view(({ title, buttonTitle, repType }) => {
+  // AROTH: don't need to use local state, use the global RepStore imported above
+  // const [repCount, setRepCount] = useState(0);
+
+  const reps = RepStore[repType];
   return (
     <>
       <Button
-        onPress={() => setRepCount(repCount + 1)}
+        onPress={() => {
+          RepStore.setReps(repType, reps + 1);
+        }}
         title={buttonTitle}
         color="steelblue"
       />
       <Text>
-        You did {repCount} {repType}
+        You did {reps} {repType}
       </Text>
       <Text>
-        You are {user.name} and you did {globalRepCount.amount} total reps this
-        workout.
+        You are {"NAME"} and you did {reps} total reps this workout.
       </Text>
     </>
   );
-};
+});
 export default repCounter;
---
+++ Stores/RepStore.js
@@ -0,0 +1,20 @@
+import { store, view } from "@risingstack/react-easy-state";
+
+const RepStore = store({
+  "Push-ups": 0,
+  "Pull-ups": 0,
+  "Sit-ups": 0,
+  "Chin-ups": 0,
+
+  setReps(name, count) {
+    this[name] = count;
+  },
+
+  get totalReps() {
+    return (
+      this["Push-ups"] + this["Pull-ups"] + this["Sit-ups"] + this["Chin-ups"]
+    );
+  },
+});
+
+export default RepStore;
