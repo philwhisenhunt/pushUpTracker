8d01aef3ae2febf4b80db644848100324710a6bc
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-01-19 20:35:57 -0500

    Remove name

--- src/FetchStuff.js
+++ src/FetchStuff.js
@@ -1,7 +1,6 @@
 // FetchUser.js
 
 export const getstuff = () => {
-  let username = name.toLowerCase().trim();
   const URL = `http://localhost:3000/exercise_sets/1`;
   return fetch(URL).then((res) => res.json());
 };
