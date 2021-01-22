16e363dcc1ac713ad9fe752071bc7c9d106fe62d
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-01-21 21:47:29 -0500

    Makin' copies

---
+++ Stores/RepStore2.js
@@ -0,0 +1,15 @@
+import { store, view } from "@risingstack/react-easy-state";
+
+const RepStore2 = store({
+  "Push-outs": 0,
+});
+
+setReps(name, count) {
+    this[name] = count;
+},
+
+get totalReps() {
+    return (
+        this
+    )
+}
