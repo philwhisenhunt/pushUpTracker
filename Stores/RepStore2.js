02b34ae6fa906519bee46a0a33c4cde60113a573
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-01-23 18:37:39 -0500

    Build out store copy a bit more

--- Stores/RepStore2.js
+++ Stores/RepStore2.js
@@ -2,17 +2,19 @@ import { store, view } from "@risingstack/react-easy-state";
 
 const RepStore2 = store({
   "Push-outs": 0,
+  "Pull-ups": 0,
+  "Sit-ups": 0,
+  "Chin-ups": 0,
 
+  setReps(name, count) {
+    this[name] = count;
+  },
 
-    setReps(name, count) {
-        this[name] = count;
-    },
-
-    get totalReps() {
-        return (
-            this["Push-ups"]
-        );
-    },
+  get totalReps() {
+    return (
+      this["Push-ups"] + this["Pull-ups"] + this["Sit-ups"] + this["Chin-ups"]
+    );
+  },
 });
 
-export default RepStore2.js
+export default RepStore2;
