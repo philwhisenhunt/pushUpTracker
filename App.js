3087edd0cf158c3e9829ebfb8d69e96853f7a5b7
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-01-30 19:52:49 -0500

    Working version saving state despite refresh

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  ActivityIndicator,
+} from "react-native";
 import RepCounter from "./Components/RepCounter";
 import { view } from "@risingstack/react-easy-state";
 import RepStore from "./Stores/RepStore";
@@ -8,54 +14,65 @@ import { saveStuff } from "./src/SaveStuff.js";
 export default view(function App() {
   const [exerciseInfo, setExerciseInfo] = useState({});
   const [serverResponse, setServerResponse] = useState({});
+
   return (
     <View style={styles.container}>
-      <View style={styles.topBlock}>
-        <Text style={styles.title}>Workout Tracker</Text>
-        <Text style={styles.title}>Tap Each Button to add a rep</Text>
-      </View>
-      <View style={styles.middleBlock}>
-        <RepCounter
-          title="Push-up tracker"
-          buttonTitle="Log a push-up"
-          repType="Push-ups"
-        />
-        <RepCounter
-          title="Pull-up tracker"
-          buttonTitle="Log a pull-up!"
-          repType="Pull-ups"
-        />
-        <RepCounter
-          title="Chin-up tracker"
-          buttonTitle="Log a chin-up"
-          repType="Chin-ups"
-        />
-        <RepCounter
-          title="Sit-up tracker"
-          buttonTitle="Log a sit-up"
-          repType="Sit-ups"
-        />
-        <Button
-          onPress={async () => {
-            const data = await saveStuff(
-              "http://localhost:3000/exercise_sets.json",
-              { rep_count: "12" }
-            );
-            setServerResponse(data);
-          }}
-          title="Save workout"
-        />
-        <Button
-          onPress={async () => {
-            const data = await getStuff();
-            setExerciseInfo(data);
-          }}
-          title="Test API"
-        />
-      </View>
-      <Text>Rep Count: {exerciseInfo.rep_count}</Text>
-      <Text>What the server sent back: {serverResponse.rep_count}</Text>
-      <Text>Total reps: {RepStore.totalReps}</Text>
+      {RepStore.isHydrated === false && (
+        <>
+          <ActivityIndicator size="large" animating={true} />
+          <Text>Please wait, loading...</Text>
+        </>
+      )}
+      {RepStore.isHydrated === true && (
+        <>
+          <View style={styles.topBlock}>
+            <Text style={styles.title}>Workout Tracker</Text>
+            <Text style={styles.title}>Tap Each Button to add a rep</Text>
+          </View>
+          <View style={styles.middleBlock}>
+            <RepCounter
+              title="Push-up tracker"
+              buttonTitle="Log a push-up"
+              repType="Push-ups"
+            />
+            <RepCounter
+              title="Pull-up tracker"
+              buttonTitle="Log a pull-up!"
+              repType="Pull-ups"
+            />
+            <RepCounter
+              title="Chin-up tracker"
+              buttonTitle="Log a chin-up"
+              repType="Chin-ups"
+            />
+            <RepCounter
+              title="Sit-up tracker"
+              buttonTitle="Log a sit-up"
+              repType="Sit-ups"
+            />
+            <Button
+              onPress={async () => {
+                const data = await saveStuff(
+                  "http://localhost:3000/exercise_sets.json",
+                  { rep_count: "12" }
+                );
+                setServerResponse(data);
+              }}
+              title="Save workout"
+            />
+            <Button
+              onPress={async () => {
+                const data = await getStuff();
+                setExerciseInfo(data);
+              }}
+              title="Test API"
+            />
+          </View>
+          <Text>Rep Count: {exerciseInfo.rep_count}</Text>
+          <Text>What the server sent back: {serverResponse.rep_count}</Text>
+          <Text>Total reps: {RepStore.totalReps}</Text>
+        </>
+      )}
     </View>
   );
 });
--- Stores/RepStore.js
+++ Stores/RepStore.js
@@ -1,6 +1,7 @@
 import { store, view } from "@risingstack/react-easy-state";
 import AsyncStorage from "@react-native-community/async-storage";
 import { observe } from "@nx-js/observer-util";
+import repCounter from "../Components/RepCounter";
 const RepStore = store({
   //Eventually, this should be seeded with stuff from the API
   "Push-ups": 0,
@@ -8,6 +9,7 @@ const RepStore = store({
   "Sit-ups": 0,
   "Chin-ups": 0,
 
+  isHydrated: false,
   persistableFields: ["Push-ups", "Pull-ups", "Sit-ups", "Chin-ups"],
 
   setReps(name, count) {
@@ -20,22 +22,25 @@ const RepStore = store({
     );
   },
 });
-console.log("Fired once");
-// console.log(AsyncStorage.getItem("REP_STORE"));
-const hydrate = async () => {
-  // console.log(await AsyncStorage.getItem("REP_STORE"));
+
+export const hydrate = async () => {
   const stringData = await AsyncStorage.getItem("REP_STORE");
   const data = JSON.parse(stringData);
-  // console.log(data);
+
   for (const [key, value] of Object.entries(data)) {
-    //iterate over the key value pairs of the object
-    // console.log(`${key}: ${value}`);
     if (RepStore.persistableFields.includes(key)) {
       RepStore.setReps(key, value);
     }
   }
+  RepStore.isHydrated = true;
+  console.log(JSON.stringify(RepStore));
 };
+// setTimeout(() => {
+//   hydrate();
+// }, 3000);
 hydrate();
+
+// Observe every change made to repStore and saving it to local storage.
 observe(() => {
   console.log("Got into observe");
   AsyncStorage.setItem("REP_STORE", JSON.stringify(RepStore));
