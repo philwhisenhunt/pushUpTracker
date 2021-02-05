66979bd8a1e5614cc74103a97923f73583510e08
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-02-04 21:11:01 -0500

    Style changes to create space

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default view(function App() {
         <>
           <View style={styles.topBlock}>
             <Text style={styles.title}>Workout Tracker</Text>
-            <Text style={styles.title}>Tap Each Button to add a rep</Text>
+            <Text style={styles.subTitle}>Tap Each Button to add a rep</Text>
           </View>
           <View style={styles.middleBlock}>
             <RepCounter
@@ -50,22 +50,30 @@ export default view(function App() {
               buttonTitle="Log a sit-up"
               repType="Sit-ups"
             />
-            <Button
-              onPress={async () => {
-                const data = await saveStuff(
-                  "http://localhost:3000/exercise_sets.json",
-                  { rep_count: "12", weight: 120, exercise_type: "kettlebell rows" }
-                );
-                setServerResponse(data);
-              }}
-              title="Save workout"
-            />
+          </View>
+          <View style={styles.fitToText}>
             <Button
               onPress={async () => {
                 const data = await getStuff();
                 setExerciseInfo(data);
               }}
-              title="Test API"
+              title="Test APII"
+              style={styles.submitButton}
+              color="black"
+            />
+            <Button
+              onPress={async () => {
+                const data = await saveStuff(
+                  "http://localhost:3000/exercise_sets.json",
+                  {
+                    rep_count: "12",
+                    weight: 120,
+                    exercise_type: "kettlebell rows",
+                  }
+                );
+                setServerResponse(data);
+              }}
+              title="Save workout"
             />
           </View>
           <Text>Rep Count: {exerciseInfo.rep_count}</Text>
@@ -85,17 +93,24 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 40,
     color: "steelblue",
-    paddingTop: 40,
+    paddingTop: 0,
+  },
+  subTitle: {
+    fontSize: 20,
+    color: "steelblue",
+    paddingTop: 0,
   },
   topBlock: {
-    flex: 1,
-    backgroundColor: "powderblue",
+    flex: 0.2,
+    backgroundColor: "black",
     justifyContent: "center",
     alignItems: "center",
+    borderWidth: 10,
   },
   middleBlock: {
     flex: 2,
     backgroundColor: "skyblue",
+    padding: 10,
   },
   bottomBlock: {
     flex: 1,
@@ -109,4 +124,14 @@ const styles = StyleSheet.create({
   //   alignSelf: "stretch",
   //   maxHeight: 100,
   // },
+  submitButton: {
+    // flex: 0.5,
+    // margin: 100,
+    padding: 10,
+    margin: 10,
+  },
+  fitToText: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
 });
