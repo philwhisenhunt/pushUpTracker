7a529a6db95fad4fa7b56e714d517fca88f176f8
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-02-08 19:58:03 -0500

    Create Profile.js

---
+++ Components/Profile.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+const LoadingScreen = () => {
+  return (
+    <View style={styles.blueBackground}>
+      <View style={styles.loadingBoxTop}>
+        <Text style={styles.innerText}>Profile</Text>
+      </View>
+    </View>
+  );
+};
+
+export default LoadingScreen;
+const styles = StyleSheet.create({
+  innerText: {
+    color: "white",
+    fontSize: 100,
+    justifyContent: "center",
+  },
+  blueBackground: {
+    backgroundColor: "skyblue",
+    flex: 1,
+  },
+  loadingBoxTop: {
+    flexDirection: "row",
+    backgroundColor: "skyblue",
+    justifyContent: "center",
+    flex: 0.5,
+    // alignSelf: "flex-end",
+  },
+  loadingBoxBottom: {
+    flexDirection: "row",
+    backgroundColor: "black",
+    justifyContent: "center",
+    flex: 0.5,
+  },
+});
