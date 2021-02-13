0ebf4db041b31c1348e2614c56744e652c691cfe
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-02-12 20:58:25 -0500

    Add input to goal screen

--- Components/GoalScreen.js
+++ Components/GoalScreen.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 const LoadingScreen = () => {
     const showAlert = () => {
         alert("Sanity check")
     }
+    const [text, setText] = useState('')
   return (
 
     <View style={styles.blueBackground}>
@@ -12,6 +14,14 @@ const LoadingScreen = () => {
       </View>
       <View style={styles.loadingBoxBottom}>
         <Text style={styles.innerText}>Goal: 300</Text>
+        <Text style={styles.innerText}>New Goal?</Text>
+        
+        <TextInput
+        style={{height: 40}}
+        placeholder="Type new goal here"
+        onChangeText={text => setText(text)}
+        defaultValue={text}
+        />
         <Button title="Save"
         onPress = {showAlert}/>
       </View>
