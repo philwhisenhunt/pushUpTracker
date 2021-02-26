c01441a13efafdc84cc6c14b0cdcd35deb00ac3f
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-02-25 22:38:10 -0500

    Copy over rest of savestuff

--- src/saveStuff2.js
+++ src/saveStuff2.js
@@ -1,3 +1,10 @@
-export async function saveStuff2(url="", data = {}){
-    
+export async function saveStuff2(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
 }
