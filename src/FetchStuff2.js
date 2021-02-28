c5960fbc38f1d31136f2faee9839e6176a4674e6
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-02-27 23:15:58 -0500

    Completed mimic

--- src/FetchStuff2.js
+++ src/FetchStuff2.js
@@ -1,3 +1,12 @@
 export const getStuff2 = async () => {
   const URL = `http://localhost:3000/exercise_sets/1.json`;
+  const response = await fetch(URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+  return data;
 };
