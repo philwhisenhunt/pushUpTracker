41f91d1db46df8f9f1708272abdfd30576cfae97
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-03-02 18:22:17 -0500

    Complete re-type of saveStuff

--- src/saveStuff3.js
+++ src/saveStuff3.js
@@ -1,6 +1,10 @@
 export async function saveStuff(url = "", data = {}) {
   const response = await fetch(url, {
     method: "POST",
-    headers: {},
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
   });
+  return response.json;
 }
