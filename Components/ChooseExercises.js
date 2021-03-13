a905de36cf49960fa62eec024b7c60530f9d9286
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-03-12 22:36:18 -0500

    Add some style

--- Components/ChooseExercises.js
+++ Components/ChooseExercises.js
@@ -1,3 +1,12 @@
 import { View, Text, Button, StyleSheet } from "react-native";
 
 const ChooseExercises = () => {};
+return <View style={StyleSheet.blueBackground}></View>;
+
+export default ChooseExercises;
+const styles = StyleSheet.create({
+  blueBackground: {
+    backgroundColor: "skyblue",
+    flex: 1,
+  },
+});
