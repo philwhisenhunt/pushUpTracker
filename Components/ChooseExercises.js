0fb65e1eb34bd2a1c6ff66603050c65e40ce3649
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-03-14 21:06:22 -0400

    Build towards exercise selector

--- Components/ChooseExercises.js
+++ Components/ChooseExercises.js
@@ -1,7 +1,13 @@
 import { View, Text, Button, StyleSheet } from "react-native";
 
 const ChooseExercises = () => {
-  return <View style={StyleSheet.blueBackground}></View>;
+  return <View style={StyleSheet.blueBackground}>
+    <Text>Add or remove exercises</Text>
+    <Text>Push-ups</Text>
+    <Text>Sit-ups</Text>
+
+
+  </View>;
 };
 export default ChooseExercises;
 const styles = StyleSheet.create({
