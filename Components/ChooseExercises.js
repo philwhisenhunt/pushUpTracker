d479535fc9d618fdac3a8735eccf5b7c2ced6f23
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-03-15 21:39:15 -0400

    Figuring out future list

--- Components/ChooseExercises.js
+++ Components/ChooseExercises.js
@@ -1,13 +1,14 @@
 import { View, Text, Button, StyleSheet } from "react-native";
 
 const ChooseExercises = () => {
-  return <View style={StyleSheet.blueBackground}>
-    <Text>Add or remove exercises</Text>
-    <Text>Push-ups</Text>
-    <Text>Sit-ups</Text>
-
-
-  </View>;
+  return (
+    <View style={StyleSheet.blueBackground}>
+      <Text>Add or remove exercises</Text>
+      <Text>Push-ups</Text>
+      <Text>Sit-ups</Text>
+      <Text>Chin-ups</Text>
+    </View>
+  );
 };
 export default ChooseExercises;
 const styles = StyleSheet.create({
@@ -16,3 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+//Use touchablewithoutfeedback?
+//https://reactnative.dev/docs/touchablewithoutfeedback
+//Add ability to add and remove stuff
