f971d65be01ae5a2842965cbc267f7052b389ea3
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-03-28 21:07:03 -0400

    Return to a state that at least shows up

--- Components/ChooseExercises.js
+++ Components/ChooseExercises.js
@@ -1,49 +1,49 @@
-import { View, Text, Button, StyleSheet } from "react-native";
-import React, { useState } from "react";
-import { TextInput } from "react-native-gesture-handler";
+// import { View, Text, Button, StyleSheet } from "react-native";
+// import React, { useState } from "react";
+// import { TextInput } from "react-native-gesture-handler";
 
-const [text, setText] = useState("");
-const ChooseExercises = () => {
-  return (
-    <View style={StyleSheet.blueBackground}>
-      <Text>Add or remove exercises</Text>
-      <TextInput
-        style={StyleSheet.blackText}
-        placeholder="Push-ups"
-        onChangeText={(text) => setText(text)}
-        defaultValue={text}
-      ></TextInput>
-      <TextInput
-        style={StyleSheet.blackText}
-        placeholder="Sit-ups"
-        onChangeText={(text) => setText(text)}
-      >
-        Sit-ups
-      </TextInput>
-      <TextInput
-        placeholder="Chin-ups"
-        onChangeText={(text) => setText(text)}
-      ></TextInput>
-      <TextInput
-        placeholder="Add exercise"
-        onChangeText={(text) => setText(text)}
-      ></TextInput>
-    </View>
-  );
-};
-export default ChooseExercises;
-const styles = StyleSheet.create({
-  blueBackground: {
-    backgroundColor: "skyblue",
-    flex: 1,
-  },
-  blackText: {
-    color: "black",
-    fontSize: 120,
-    justifyContent: "flex-start",
-  },
-});
-//Use touchablewithoutfeedback?
-//https://reactnative.dev/docs/touchablewithoutfeedback
-//Add ability to add and remove stuff
-//Would each block of text have to be in a view or different type of component?
+// const [text, setText] = useState("");
+// const ChooseExercises = () => {
+//   return (
+//     <View style={StyleSheet.blueBackground}>
+//       <Text>Add or remove exercises</Text>
+//       <TextInput
+//         style={StyleSheet.blackText}
+//         placeholder="Push-ups"
+//         onChangeText={(text) => setText(text)}
+//         defaultValue={text}
+//       ></TextInput>
+//       <TextInput
+//         style={StyleSheet.blackText}
+//         placeholder="Sit-ups"
+//         onChangeText={(text) => setText(text)}
+//       >
+//         Sit-ups
+//       </TextInput>
+//       <TextInput
+//         placeholder="Chin-ups"
+//         onChangeText={(text) => setText(text)}
+//       ></TextInput>
+//       <TextInput
+//         placeholder="Add exercise"
+//         onChangeText={(text) => setText(text)}
+//       ></TextInput>
+//     </View>
+//   );
+// };
+// export default ChooseExercises;
+// const styles = StyleSheet.create({
+//   blueBackground: {
+//     backgroundColor: "skyblue",
+//     flex: 1,
+//   },
+//   blackText: {
+//     color: "black",
+//     fontSize: 120,
+//     justifyContent: "flex-start",
+//   },
+// });
+// //Use touchablewithoutfeedback?
+// //https://reactnative.dev/docs/touchablewithoutfeedback
+// //Add ability to add and remove stuff
+// //Would each block of text have to be in a view or different type of component?
