166dc79392daca64bc4af6e012f40a3ad243c324
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-05-28 22:13:58 -0400

    Begin switch to edit screen

---
+++ Components/EditProfileScreen.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+const ProfileScreen = () => {
+    return(
+
+    )
+}
