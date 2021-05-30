19637870292137a4a39ca4e7d42643e670ae79af
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-05-29 23:36:21 -0400

    Add more to edit profile screen

--- Components/EditProfileScreen.js
+++ Components/EditProfileScreen.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
-const ProfileScreen = () => {
-    return(
+const EditProfileScreen = () => {
+  return <Text>Edit Profile</Text>;
+};
 
-    )
-}
+export default EditProfileScreen;
