d4bcbfb65928cbf9ea3514a6fa1f60465a558a80
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-05-31 10:44:39 -0400

    Add smaller stats section

--- Components/EditProfileScreen.js
+++ Components/EditProfileScreen.js
@@ -8,7 +8,10 @@ const EditProfileScreen = () => {
       <Text>Edit Profile</Text>
       <Text>Phil W.</Text>
       <Text>10 Achievements</Text>
-      <Text>Avg reps per day</Text>
+      <View>
+        <Text>Avg reps per day</Text>
+        <Text>10</Text>
+      </View>
     </View>
   );
 };
@@ -18,5 +21,8 @@ export default EditProfileScreen;
 const styles = StyleSheet.create({
   headline: {
     color: "black",
+  },
+  smallStats: {
+    fontSize: 20,
   },
 });
