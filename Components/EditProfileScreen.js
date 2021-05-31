b7f992f3d33c44dc42539c400b666c5bf3e46b55
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-05-30 21:22:23 -0400

    Add text and view to edit profile page

--- Components/EditProfileScreen.js
+++ Components/EditProfileScreen.js
@@ -3,7 +3,20 @@ import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
 const EditProfileScreen = () => {
-  return <Text>Edit Profile</Text>;
+  return (
+    <View style={styles.headline}>
+      <Text>Edit Profile</Text>
+      <Text>Phil W.</Text>
+      <Text>10 Achievements</Text>
+      <Text>Avg reps per day</Text>
+    </View>
+  );
 };
 
 export default EditProfileScreen;
+
+const styles = StyleSheet.create({
+  headline: {
+    color: "black",
+  },
+});
