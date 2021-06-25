3a7af874392e4025f4fafd12149f2ab9cf62ccbe
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-06-24 22:13:37 -0400

    Create WorkoutGraphScreen.js

---
+++ Components/WorkoutGraphScreen.js
@@ -0,0 +1,10 @@
+import { ViewBase, Text, Button, StyleSheet } from "react-native";
+
+
+const WorkoutGraphScreen = () => {
+    return (
+        <View>
+            <Text>Workout Progress - 30 days</Text>
+        </View>
+    )
+}
