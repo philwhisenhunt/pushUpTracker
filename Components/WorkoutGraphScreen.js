cad528ad8d711d28d27f3a8720fc64b0f4f04e65
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-06-25 22:06:15 -0400

    Prep for Adjustable calendar

--- Components/WorkoutGraphScreen.js
+++ Components/WorkoutGraphScreen.js
@@ -4,7 +4,10 @@ import { ViewBase, Text, Button, StyleSheet } from "react-native";
 const WorkoutGraphScreen = () => {
     return (
         <View>
-            <Text>Workout Progress - 30 days</Text>
+            <Text>Workout Progress</Text>
+                <TouchableOpacity>
+                    <Text>30 days</Text>
+                </TouchableOpacity>
         </View>
     )
 }
