d713d8e180450934f59489ddc8a68fd56ff134d0
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-07-11 21:32:30 -0400

    Add to url

--- src/FetchTotalReps.js
+++ src/FetchTotalReps.js
@@ -1,3 +1,12 @@
 export const fetchTotalReps = async () => {
-    const URL = `http://localhost:3000/`
+    const URL = `http://localhost:3000/exercises?may+2021`;
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    const data = await response.json();
+    return data;
 }
