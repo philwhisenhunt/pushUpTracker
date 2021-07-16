fe513e2d1213e87254483b340b6fe1f4e456070c
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-07-15 21:39:36 -0400

    Add concatenation

--- src/FetchTotalReps.js
+++ src/FetchTotalReps.js
@@ -1,16 +1,19 @@
 export const fetchTotalReps = async () => {
+    //Set the time frame based on params
     let timeframe = "may+2021"
+    //add to the url
+    const URL = `http://localhost:3000/exercises?`;
+
+    //set whatever is returned and decode if needed
     //interpolate timeframe
-    const URL = `http://localhost:3000/exercises?may+2021`;
+    const URL = `http://localhost:3000/exercises` + timeframe;
     const response = await fetch(url, {
         method: "GET",
         headers: {
             "Content-Type": "application/json",
         },
     });
-//Set the time frame based on params
-//add to the url
-//set whatever is returned and decode if needed
+
     const data = await response.json();
     return data;
 }
