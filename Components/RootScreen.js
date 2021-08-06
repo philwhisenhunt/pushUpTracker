d675a20b0255dad0b501c084029c65d6ae806e35
Phil Whisenhunt <philip.whisenhunt@gmail.com>
2021-08-05 23:43:42 -0400

    Trying to match guide

--- Components/RootScreen.js
+++ Components/RootScreen.js
@@ -111,8 +111,8 @@ const RootScreen = view(() => {
               let one = exercise_list[0];
               let two = exercise_list[1];
               let three = exercise_list[2];
-              //  exercise_list.map(list => 
-              //   (puts list)
+              //  exercise_list.map((list) => 
+              //   (puts {list})
               // )
               const data = await LongtermSave(
                 "http://localhost:3000/exercise_set/*.json",
